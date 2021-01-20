5336b040c94ca6cff48811b71a6327efe4989b62
ravijoshi1106 <ravi.bh.java@gmail.com>
2021-01-20 16:34:38 +0530

    second commit

--- src/main/java/com/nxsol/controller/ProjectController.java
+++ src/main/java/com/nxsol/controller/ProjectController.java
@@ -61,5 +61,6 @@ public class ProjectController {
 	public ResponseEntity<?> deleteProject(@PathVariable Long id) throws Exception {
 		projectService.deleteProject(id);
 		return new ResponseEntity<>(HttpStatus.OK);
+		
 	}
 }

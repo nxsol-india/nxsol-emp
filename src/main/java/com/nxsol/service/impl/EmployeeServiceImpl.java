613f3bdc939b0f98c82f4999ad722fa2a8fe75e5
meena <meena@meena-Vostro-3549>
2021-01-20 16:54:57 +0530

    code commited

--- src/main/java/com/nxsol/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/nxsol/service/impl/EmployeeServiceImpl.java
@@ -44,7 +44,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 			}
 
 			employeeDto.setProjectDto(projectDtos);
-			employeeDtos.add(employeeDto);
+			employeeDtos.add(employeeDto); 
 		}
 		return employeeDtos;
 	}
